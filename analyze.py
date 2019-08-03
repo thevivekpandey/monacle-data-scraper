63e09e7af306259e50289a347321cde5c6af3091
Vivek Pandey <thevivekpandey@gmail.com>
2019-08-03 10:30:21 +0000

    more analysis

--- analyze.py
+++ analyze.py
@@ -1,6 +1,36 @@
 import json
 from mo_mongo import MoMongo
+from datetime import datetime
+from datetime import timedelta
+from datetime import date
 #from evaluator import high_cpu_util_yesterday
+
+def check_for_single_anomaly(time_series, ref_date):
+    sums_counts = {}
+    for t, val in time_series.items():
+        d = t.date()
+        sums_counts.setdefault(d, {'sum': 0, 'count': 0})
+        sums_counts[d]['sum'] += val
+        sums_counts[d]['count'] += 1
+    averages = {}
+    for d, detail in sums_counts.items():
+        averages[d] = detail['sum'] / detail['count']
+    print(averages)
+
+def check_for_anomalies(grouped_data, tz, ref_date):
+    for dim_val, time_series in grouped_data.items():
+        if dim_val == 'i-0fedbd54d59c494be':
+            print("min:", min(time_series.keys()))
+            print("max:", max(time_series.keys()))
+            print("len:", len(time_series))
+    #tz is not used currently
+    anomalies = []
+    for dim_val, time_series in grouped_data.items():
+        if dim_val == 'i-0fedbd54d59c494be':
+            anomaly = check_for_single_anomaly(time_series, ref_date)
+            if anomaly:
+                anomalies.append(anomaly)
+    return anomalies
 
 def group_by_dimension(data, dimension):
     grouped_data = {}
@@ -22,7 +52,10 @@ if __name__ == '__main__':
             metric_name = rule['metricName']
             period = rule['period']
             function = rule['function_name']
-        data = mo_mongo.get_data_from_mongo(namespace, dimension, metric_name)
+        start_time = datetime.now() - timedelta(seconds=period + 86400)
+        data = mo_mongo.get_data_from_mongo(namespace, dimension, metric_name, start_time)
         grouped_data = group_by_dimension(data, 'dimVal')
-        print(grouped_data)
+        #print(grouped_data)
+        anomalies = check_for_anomalies(grouped_data, 'Asia/Taipei', date.today())
+        print(anomalies)
 
--- mo_mongo.py
+++ mo_mongo.py
@@ -5,8 +5,9 @@ class MoMongo():
         self.conn = MongoClient('localhost')
         self.data = self.conn['test'].data
 
-    def get_data_from_mongo(self, ns, dim_name, metric):
+    def get_data_from_mongo(self, ns, dim_name, metric, start_time):
         query = {'ns': ns, 'metric': metric, 'dimName': dim_name}
+        query['ts'] = {'$gt': start_time}
         projection = {'ts': 1, 'metricVal': 1, 'dimVal': 1}
         results = self.data.find(query, projection)
         data =  [result for result in results]
