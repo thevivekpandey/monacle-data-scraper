d18baa4c3b68bf23194db9b96600c2aabe91e18d
Vivek Pandey <thevivekpandey@gmail.com>
2019-09-06 18:25:33 +0000

    demo data

--- put_demo_data.py
+++ put_demo_data.py
@@ -3,6 +3,11 @@ import re
 from database import Database
 from datetime import datetime, timedelta
 
+def format_monospace(in_text, match):
+    output = '<span style="font-style: monospace"><a href="#">' + match[1:-1] + '</a></span>'
+    x = in_text.replace(match, output)
+    return x
+
 def insert_one_anomaly(fname, db):
     with open(fname) as f:
         chart_data = f.read()
@@ -20,7 +25,12 @@ def insert_one_anomaly(fname, db):
             output = datetime.now() - timedelta(days=int(delta))
             output_str = output.strftime("%b %d")
         chart_data = chart_data.replace(match, output_str)
-    
+
+    regex = r'\[[a-zA-Z0-9_-]+\]'
+    matches = re.findall(regex, chart_data)
+    for match in matches:
+        chart_data = format_monospace(chart_data, match)
+
     chart_data = yaml.load(chart_data)
     
     db.write_anomaly(chart_data)
@@ -28,5 +38,8 @@ def insert_one_anomaly(fname, db):
 if __name__ == '__main__':
     db = Database(13)
     db.clear_anomalies()
-    insert_one_anomaly('demo_data.yml', db)
+    insert_one_anomaly('demo_data_1.yml', db)
     insert_one_anomaly('demo_data_2.yml', db)
+    insert_one_anomaly('demo_data_3.yml', db)
+    insert_one_anomaly('demo_data_4.yml', db)
+    insert_one_anomaly('demo_data_5.yml', db)
